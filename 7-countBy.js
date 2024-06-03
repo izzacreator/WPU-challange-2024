40202ef8961435b9759dd7c3e1513b5d7db60dde
IzzaIndo <izzacreator@gmail.com>
2024-06-03 12:39:16 +0700

    day #7

---
+++ 7-countBy.js
@@ -0,0 +1,20 @@
+// WPU challange 2024
+// 7/366
+
+// function countBy(x, n) {
+//   let z = [];
+//   for (let i = 1; i <= n; i++) {
+//     z.push(i * x);
+//   }
+//   return z;
+// }
+
+
+// function countBy(x, n) {
+//   return [...Array(n)].map((el, i) => (i + 1) * x );
+// }
+
+
+const countBy = (x, n) => [...Array(n)].map((el, i) => (i + 1) * x);
+
+console.log(countBy(2, 5));
